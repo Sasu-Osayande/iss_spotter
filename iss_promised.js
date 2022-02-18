b8e30a866bac9d7a9feb160182ff46e422e094ef
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-18 07:07:48 -0500

    Defined and exported the three functions and returned the requests as promises

---
+++ iss_promised.js
@@ -0,0 +1,22 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = () => {
+  return request('https://api.ipify.org?format=json');
+};
+
+/* 
+ * Makes a request to freegeoip.app using the provided IP address, to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+const fetchCoordsByIP = body => {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`)
+};
+
+const fetchISSFlyOverTimes = body => {
+  const { latitude, longitude } = JSON.parse(body);
+  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`)
+}
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
