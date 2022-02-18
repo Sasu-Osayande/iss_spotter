f297a86ecffbff808b3194bf9784d8e14a3fdb0c
Sasu Osayande <osayande.sasu@outlook.com>
2022-02-18 07:43:04 -0500

    Exported nextISSTimesForMyLocation which chains the other functions and returns the final results

--- iss_promised.js
+++ iss_promised.js
@@ -19,4 +19,14 @@ const fetchISSFlyOverTimes = body => {
   return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`)
 }
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+const nextISSTimesForMyLocation = () => {
+  return fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(data => {
+    const { response } = JSON.parse(data);
+    return response
+  });
+};
+
+module.exports = { nextISSTimesForMyLocation};
